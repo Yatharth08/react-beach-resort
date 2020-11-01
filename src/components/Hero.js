cf766e6872ce07cc9f4ab9f0ab7911996ba675c5
Yatharth08 <thakuryatharth1@gmail.com>
2020-11-01 19:43:20 +0530

    first commit

---
+++ src/components/Hero.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Hero(props) {
+    return (
+        <header className={props.hero}>
+            {props.children}
+        </header>
+    )
+}
---
+++ src/pages/Rooms.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Hero from '../components/Hero';
+import {Link} from 'react-router-dom';
+import Banner from '../components/Banner';
+import RoomContainer from '../components/RoomContainer';
+export default function Rooms() {
+    return (
+        <>
+            <Hero hero="roomsHero">
+                <Banner title="Our Rooms">
+                    <Link to="/" className="btn-primary">
+                        return home
+                    </Link>
+                </Banner>
+            </Hero>
+            <RoomContainer/>
+        </>
+    )
+}
